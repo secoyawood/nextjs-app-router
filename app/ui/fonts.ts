6b2d559d5d7f2140429d7a1d3e4910c2496b4885
Secoya Wood <you@example.com>
2025-02-28 09:15:33 -0800

    fixed font subset error

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,4 +2,7 @@ import { Inter } from 'next/font/google';
 import { Lusitana } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
-export const lusitana = Lusitana({ weight: ['400', ] });
+export const lusitana = Lusitana({
+    subsets: ['latin'],
+    weight: ['400', '700'], // or other weights you are using.
+  });
